b7b21d6806e4d8b4c5c336f7d21f9b7325085971
RayanAlim <61893163+RayanAlim@users.noreply.github.com>
2021-01-02 23:42:08 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+
+music_data = pd.read_csv('music.csv') 
+X = music_data.drop(columns=['genre']) 
+Y = music_data['genre']
+
+model = DecisionTreeClassifier() 
+model.fit(X, Y)
+
+
+tree.export_graphviz(model, out_file='music-recommender.dot',
+                      feature_names = ['age', 'gender'],
+                      class_names=sorted(Y.unique()),
+                       label = 'all',
+                      rounded=True)
